fb4d875ef36d01cf73be738b7a1cddb0c1e92535
Fabrice Gangler <fabrice@asqatasun.org>
2017-08-06 09:50:16 +0200

    Rgaa3.2017, enabled 5.1.1 from Rgaa3.2016: same ID / same content
    ----> enabled in database
    
    rule 5.1.1: same ID and same content
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa-accessibilite/changelog.html
    
    source files:
    /rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule050101.java
    /rules/rules-rgaa3.2016/src/test/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule050101Test.java
    /rules/rules-rgaa3.2016/src/test/resources/testcases/rgaa32016/Rgaa32016Rule050101/*
    /documentation/en/90_Rules/rgaa3.2016/05.Tables/Rule-5-1-1.md

--- rules/rules-rgaa3.2017/src/main/resources/sql/rgaa3.2017-insert.sql
+++ rules/rules-rgaa3.2017/src/main/resources/sql/rgaa3.2017-insert.sql
@@ -667,3 +667,5 @@ UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='rgaa32017-2-1-1';
 
 UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='rgaa32017-2-2-1';
 
+UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='rgaa32017-5-1-1';
+
